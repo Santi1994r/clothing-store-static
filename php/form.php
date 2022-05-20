d29d276a63073a1cc89adb3400555ae682ea609a
santiago ruiz <santiagoruiz9416@gmail.com>
2022-05-20 02:25:17 -0300

    creo carpeta php y archivo exito.html

---
+++ php/form.php
@@ -0,0 +1,21 @@
+<?php
+
+$nombre = $_POST["name"];
+$apellido = $_POST["surname"];
+$mail = $_POST["email"];
+$telefono = $_POST["phone"];
+$provincia = $_POST["province"];
+$localidad = $_POST["location"];
+
+//lo envio a mi mail personal
+
+$para = "santiagoruiz9416@gmail.com";
+$asunto = "Este email fue enviado desde Urbanos Clothing";
+
+//Funcion
+mail($para, $asunto, utf8_decode($nombre, $apellido, $mail, $telefono, $provincia, $localidad));
+
+//Funcion para reedireccionar al usuario una ves enviado sus datos por el formulario
+header("location:exito.html");
+
+?>
